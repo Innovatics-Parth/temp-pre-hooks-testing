6b68ebbee2a2bb0ab477265341daae22d5b237c8
Parth <parthshah@teaminnovatics.com>
2025-06-05 10:15:31 +0000

    [UPD] Updated code added pre commits configs

--- scripts/sample_file.py
+++ scripts/sample_file.py
@@ -5,11 +5,11 @@ import pdb
 
 
 def sample_func():
-    print("Hello World")    
+    print("Hello World")
     pdb.set_trace()  # Should be caught by debug-statements
     x = "Hello 'world'"  # Should be fixed by double-quote-string-fixer
     return x
-    
+
 # The test function below should be flagged by name-tests-test
 def sample_test_func():
-    assert sample_func() == "Hello 'world'"    
+    assert sample_func() == "Hello 'world'"
